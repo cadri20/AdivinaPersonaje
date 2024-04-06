ef8e6626963578e89d2e89ff01f6a3fdef80f4e9
cadri20 <cadrian_2001@hotmail.com>
2024-04-06 18:48:01 -0500

    Version preliminar

---
+++ Model/Adivinanza.cs
@@ -0,0 +1,30 @@
+﻿namespace AdivinaPersonaje.Model
+{
+    public record Adivinanza
+    {
+        public int Id { get; set; }
+        public List<string> Respuestas { get; set; }
+        public List<string> Pistas { get; set; }
+
+        public Adivinanza(List<string> respuestas, List<string> pistas)
+        {
+            Respuestas = respuestas;
+            Pistas = pistas;
+        }
+
+        public bool EsCorrecta(string respuesta)
+        {
+            bool esCorrecta = false;
+            foreach (var r in Respuestas)
+            {
+                if (r.Equals(respuesta, StringComparison.OrdinalIgnoreCase))
+                {
+                    esCorrecta = true;
+                    break;
+                }
+            }
+
+            return esCorrecta;
+        }
+    }
+}
---
+++ Services/AdivinanzasService.cs
@@ -0,0 +1,27 @@
+﻿using AdivinaPersonaje.Model;
+using System.Net.Http.Json;
+
+namespace AdivinaPersonaje.Services
+{
+    public interface IAdivinanzasService
+    {
+        Task<Adivinanza?> GetAdivinanza(int id);
+    }
+
+    public class AdivinanzasService(HttpClient httpClient) : IAdivinanzasService
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        private List<Adivinanza>? adivinanzas;
+        public async Task<Adivinanza?> GetAdivinanza(int id)
+        {
+            if(adivinanzas == null)
+            {
+                adivinanzas = await _httpClient.GetFromJsonAsync<List<Adivinanza>>("adivinanzas.json");
+            }
+
+            return adivinanzas?.Find(a => a.Id == id);
+        }
+
+    }
+}
